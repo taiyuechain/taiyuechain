cebcfd612fb6d66c178783d39ef824dd367a9fe6
BiyunZeng <chhaozeng@163.com>
2020-03-12 21:41:28 +0800

    add function ForbidAddress

--- core/types/contractUtil.go
+++ core/types/contractUtil.go
@@ -3,9 +3,23 @@ package types
 import (
 	"github.com/ethereum/go-ethereum/common"
 
+	"bytes"
+	"errors"
+	"fmt"
 )
 
 
 var (
 	CACertListAddress = common.BytesToAddress([]byte("CACertList"))
 )
+
+var(
+	ErrForbidAddress     = errors.New("Forbidding Address")
+)
+
+func ForbidAddress(addr common.Address) error {
+	if bytes.Equal(addr[:], CACertListAddress[:]) {
+		return errors.New(fmt.Sprint("addr error:", addr, ErrForbidAddress))
+	}
+	return nil
+}
