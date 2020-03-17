c1776f7f2b631e1359535b4b1a477325c9f606c6
caoliangccut20121192 <542080720@qq.com>
2020-03-17 20:04:54 +0800

    keyexchange modify import library,libraryis is not local library

--- crypto/gm/sm2/keyexchange.go
+++ crypto/gm/sm2/keyexchange.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
-	"github.com/ZZMarquis/gm/sm3"
-	"github.com/ZZMarquis/gm/util"
+	"github.com/taiyuechain/taiyuechain/crypto/gm/sm3"
+	"github.com/taiyuechain/taiyuechain/crypto/gm/util"
 	"hash"
 	"math/big"
 )
