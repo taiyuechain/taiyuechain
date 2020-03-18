59a99694e860e35409f95b47b457fc1e0e0dbdce
caoliangccut20121192 <542080720@qq.com>
2020-03-18 22:37:15 +0800

    crypto\taiCrypto\taiTypes.go: add 256bit hash implement

--- crypto/taiCrypto/taiTypes.go
+++ crypto/taiCrypto/taiTypes.go
@@ -1,10 +1,13 @@
 package taiCrypto
 
 import (
-	"reflect"
-	"github.com/taiyuechain/taiyuechain/core"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/taiyuechain/taiyuechain/core"
+	tycrpto "github.com/taiyuechain/taiyuechain/crypto"
+	"github.com/taiyuechain/taiyuechain/crypto/gm/sm3"
 	"math/big"
+	"reflect"
 )
 
 // Lengths of hashes and addresses in bytes.
@@ -13,10 +16,7 @@ const (
 	TaiHashLength = 64
 	// AddressLength is the expected length of the address
 	TaiAddressLength = 20
-
-
 )
-
 
 var (
 	hashT    = reflect.TypeOf(THash{})
@@ -25,15 +25,39 @@ var (
 
 type THash [TaiHashLength]byte
 type TAddress [TaiAddressLength]byte
+type TaiHash interface {
+	Keccak256Hash(data []byte) THash
+}
 
+func (t THash) Keccak256Hash(data []byte) THash {
+	var tHash THash
+	switch core.HashCryptoType {
+	case core.HASHCRYPTOSM3:
+		//TODO do GM hash
+		sm := sm3.New()
+		sm.Write(data)
+		s := sm.Sum(nil)
+		fmt.Println(len(s))
+		for i := 32; i < TaiHashLength; i++ {
+			tHash[i-32] = s[i-32]
+		}
+		return tHash
 
+	case core.HASHCRYPTOHAS3:
+		tyhash := tycrpto.Keccak256Hash(data)
+		return gjHashToGmHash(tyhash)
+
+	}
+	return tHash
+}
 
 func BytesToHash(b []byte) THash {
 
 	var tHash THash
-	switch core.HashCryptoType{
+	switch core.HashCryptoType {
 	case core.HASHCRYPTOSM3:
 		//TODO do GM hash
+
 	case core.HASHCRYPTOHAS3:
 		gjhash := common.BytesToHash(b)
 		tHash = gjHashToGmHash(gjhash)
@@ -41,7 +65,7 @@ func BytesToHash(b []byte) THash {
 	return tHash
 }
 
-func BigToHash(b *big.Int) THash{
+func BigToHash(b *big.Int) THash {
 	var tHash THash
 	switch core.HashCryptoType {
 	case core.HASHCRYPTOSM3:
@@ -55,11 +79,10 @@ func BigToHash(b *big.Int) THash{
 
 }
 
-func gjHashToGmHash(b common.Hash)THash{
+func gjHashToGmHash(b common.Hash) THash {
 	var tHash THash
-	for i:=32;i< TaiHashLength;i++ {
-		tHash[i] = b[i-32]
+	for i := 32; i < TaiHashLength; i++ {
+		tHash[i-32] = b[i-32]
 	}
 	return tHash
 }
-
