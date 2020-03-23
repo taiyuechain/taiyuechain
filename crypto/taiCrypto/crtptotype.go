843864a2e476f01a296d3e9e022f4d3209f1c45e
caoliangccut20121192 <542080720@qq.com>
2020-03-23 11:08:12 +0800

    taiyuechain/crypto/taiCrypto/crtptotype.go: Judging international algorithm and national secret algorithm

---
+++ crypto/taiCrypto/crtptotype.go
@@ -0,0 +1,14 @@
+package taiCrypto
+
+var SymmetricCryptoType uint8
+var AsymmetricCryptoType uint8
+var HashCryptoType uint8
+
+const (
+	SYMMETRICCRYPTOSM4    = 1
+	SYMMETRICCRYPTOAES    = 2
+	ASYMMETRICCRYPTOECDSA = 3
+	ASYMMETRICCRYPTOSM2   = 4
+	HASHCRYPTOSM3         = 5
+	HASHCRYPTOHAS3        = 6
+)
