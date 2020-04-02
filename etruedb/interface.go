48d17191a2c50e412e95f5f6a2328d540d03f1c5
caoliangccut20121192 <542080720@qq.com>
2020-04-02 17:05:52 +0800

    taiyuechain/etruedb/interface.go:add keyValueReade implement

--- etruedb/interface.go
+++ etruedb/interface.go
@@ -16,9 +16,61 @@
 
 package etruedb
 
+import "io"
+
 // Code using batches should try to add this much data to the batch.
 // The value was determined empirically.
 const IdealBatchSize = 100 * 1024
+
+//caoliang add
+type KeyValueReader interface {
+	// Has retrieves if a key is present in the key-value data store.
+	Has(key []byte) (bool, error)
+
+	// Get retrieves the given key if it's present in the key-value data store.
+	Get(key []byte) ([]byte, error)
+}
+
+// KeyValueWriter wraps the Put method of a backing data store.
+type KeyValueWriter interface {
+	// Put inserts the given value into the key-value data store.
+	Put(key []byte, value []byte) error
+
+	// Delete removes the key from the key-value data store.
+	Delete(key []byte) error
+}
+
+// Stater wraps the Stat method of a backing data store.
+type Stater interface {
+	// Stat returns a particular internal stat of the database.
+	Stat(property string) (string, error)
+}
+
+// Compacter wraps the Compact method of a backing data store.
+type Compacter interface {
+	// Compact flattens the underlying data store for the given key range. In essence,
+	// deleted and overwritten versions are discarded, and the data is rearranged to
+	// reduce the cost of operations needed to access them.
+	//
+	// A nil start is treated as a key before all keys in the data store; a nil limit
+	// is treated as a key after all keys in the data store. If both is nil then it
+	// will compact entire data store.
+	Compact(start []byte, limit []byte) error
+}
+
+// KeyValueStore contains all the methods required to allow handling different
+// key-value data stores backing the high level database.
+type KeyValueStore interface {
+	KeyValueReader
+	KeyValueWriter
+	Batcher
+	Iteratee
+	Stater
+	Compacter
+	io.Closer
+}
+
+//caoliang add
 
 // Putter wraps the database write operation supported by both batches and regular databases.
 type Putter interface {
@@ -34,10 +86,18 @@ type Deleter interface {
 type Database interface {
 	Putter
 	Deleter
+	//KeyValueWriter
+	//KeyValueReader
 	Get(key []byte) ([]byte, error)
 	Has(key []byte) (bool, error)
 	Close()
 	NewBatch() Batch
+	Stater
+	Compacter
+	AncientReader
+	AncientWriter
+	//Iteratee
+
 }
 
 // Batch is a write-only database that commits changes to its host database
@@ -45,8 +105,34 @@ type Database interface {
 type Batch interface {
 	Putter
 	Deleter
+	//KeyValueWriter
+	KeyValueReader
 	ValueSize() int // amount of data in the batch
 	Write() error
 	// Reset resets the batch for reuse
 	Reset()
+	Replay(w KeyValueWriter) error
+}
+
+/*type Batch interface {
+	KeyValueWriter
+
+	// ValueSize retrieves the amount of data queued up for writing.
+	ValueSize() int
+
+	// Write flushes any accumulated data to disk.
+	Write() error
+
+	// Reset resets the batch for reuse.
+	Reset()
+
+	// Replay replays the batch contents.
+	Replay(w KeyValueWriter) error
+}*/
+
+// Batcher wraps the NewBatch method of a backing data store.
+type Batcher interface {
+	// NewBatch creates a write-only database that buffers changes to its host db
+	// until a final write is called.
+	NewBatch() Batch
 }
