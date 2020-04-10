1b65d340e43db07fbf75e97dc516496e5d06a2c3
Ashton Chen <cdljsj@163.com>
2020-04-11 07:43:06 +0800

    fix downloader unit test issue

--- etrue/sync.go
+++ etrue/sync.go
@@ -210,7 +210,6 @@ func (pm *ProtocolManager) synchronise(peer *peer) {
 		}
 	}
 	// Run the sync cycle, and disable fast sync if we've went past the pivot block
-	//TODO
 	if err := pm.downloader.Synchronise(peer.id, pHead, pTd, mode); err != nil {
 		return
 	}
