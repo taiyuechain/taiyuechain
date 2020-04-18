146c4057cf4cae5576de7df212c72eeb8d3f5dca
BiyunZeng <chhaozeng@163.com>
2020-04-18 14:18:15 +0800

    keystore/keystore_plain_test.go: fix build error

--- accounts/keystore/keystore_plain_test.go
+++ accounts/keystore/keystore_plain_test.go
@@ -198,7 +198,8 @@ func TestV1_2(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	privHex := hex.EncodeToString(crypto.FromECDSA(k.PrivateKey))
+	//var kk  *ecs
+	privHex := hex.EncodeToString(crypto.FromECDSA(&k.PrivateKey.Private))
 	expectedHex := "d1b1178d3529626a1a93e073f65028370d14c7eb0936eb42abef05db6f37ad7d"
 	if privHex != expectedHex {
 		t.Fatal(fmt.Errorf("Unexpected privkey: %v, expected %v", privHex, expectedHex))
