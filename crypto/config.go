88a888463ac3887e58c88ce9a835986f35b62d7c
caoliangccut20121192 <542080720@qq.com>
2020-05-11 16:46:08 +0800

    crypto:modify config cryptotype

--- crypto/config.go
+++ crypto/config.go
@@ -1,13 +1,13 @@
 package crypto
 
 const (
-	CRYPTO_P256_SH3 = 1 //GUOJI HASH AND   Asymmetric Encryption
-	CRYPTO_SM2_SM3  = 2 //GUOMI hash and  Asymmetric Encryption;
-	CRYPTO_S256_SH3 = 3
+	CRYPTO_P256_SH3_AES = 1 //GUOJI HASH AND   Asymmetric Encryption
+	CRYPTO_SM2_SM3_SM4  = 2 //GUOMI hash and  Asymmetric Encryption;
+	CRYPTO_S256_SH3_AES = 3
 )
 
-var cryptotype uint8
+var cryptotype = CRYPTO_SM2_SM3_SM4
 
 func SetCrtptoType(cryptoType uint8) {
-	cryptotype = cryptoType
+	cryptotype = int(cryptoType)
 }
