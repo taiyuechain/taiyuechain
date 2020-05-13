ef7dd87ba9f987c9807241cf89f9d55938f2ed13
jiyilanzhou <450595468@qq.com>
2020-05-13 11:33:26 +0800

    discover: fix trueVersion to 515

--- p2p/discover/udp.go
+++ p2p/discover/udp.go
@@ -57,7 +57,7 @@ const (
 	ntpFailureThreshold = 32               // Continuous timeouts after which to check NTP
 	ntpWarningCooldown  = 10 * time.Minute // Minimum amount of time to pass before repeating NTP warning
 	driftThreshold      = 10 * time.Second // Allowed clock drift before warning user
-	trueVersion         = 520
+	trueVersion         = 515
 	checkIPTime         = 30 * time.Second
 
 	// Discovery packets are defined to be no larger than 1280 bytes.
