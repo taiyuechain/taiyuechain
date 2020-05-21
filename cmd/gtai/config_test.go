bc099df0160dab31eb9c76b785cd4d5a9d600bc5
BiyunZeng <chhaozeng@163.com>
2020-05-21 14:49:47 +0800

    cmd/gtai/config.toml: updata config.toml and add read config.toml file test

---
+++ cmd/gtai/config_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+	"github.com/taiyuechain/taiyuechain/crypto"
+	"encoding/hex"
+)
+
+
+func TestConfigTomlFile(t *testing.T)   {
+
+	cfg := gethConfig{}
+	file := "./config.toml"
+	if err := loadConfig(file, &cfg); err != nil {
+		fmt.Println(err)
+		t.Fatalf("load config fale")
+	}
+	pubk,err := crypto.GetPubByteFromCert(cfg.Etrue.Genesis.CertList[0])
+	if err!=nil{
+		fmt.Println(err)
+		t.Fatalf("cer1")
+	}
+	fmt.Println(hex.EncodeToString(pubk))
+
+	fmt.Println("---the commite")
+	fmt.Println(hex.EncodeToString(cfg.Etrue.Genesis.Committee[0].Coinbase[:]))
+	fmt.Println(hex.EncodeToString(cfg.Etrue.Genesis.Committee[1].Coinbase[:]))
+	//fmt.Println(cfg.Etrue.Genesis.Alloc["0xbD1edee3bdD812BB5058Df1F1392dDdd99dE58cc"])
+}
