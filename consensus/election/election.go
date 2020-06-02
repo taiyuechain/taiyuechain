7125f1d506ae522b74f80b32b8558f974ba50ad3
jasonberger11 <icefish0012@gmail.com>
2020-06-02 20:34:00 +0800

    election.go: update GetEpochHeigth

--- consensus/election/election.go
+++ consensus/election/election.go
@@ -1494,7 +1494,7 @@ func GetEpochIDFromHeight(height *big.Int) *big.Int {
 	return new(big.Int).Div(height, big.NewInt(int64(EpochSize)))
 }
 func GetEpochHeigth(eid *big.Int) (*big.Int, *big.Int) {
-	begin := new(big.Int).Mul(eid, NewInt(int64(EpochSize)))
+	begin := new(big.Int).Mul(eid, big.NewInt(int64(EpochSize)))
 	return begin, new(big.Int).Add(begin, big.NewInt(int64(EpochSize-1)))
 }
 
