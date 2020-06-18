1a125a6cf87b8f3aab387274770b4a80d6fab548
jiyilanzhou <450595468@qq.com>
2020-06-18 20:54:27 +0800

    vm/permission_test: update permission test

--- core/vm/permission_table_test.go
+++ core/vm/permission_table_test.go
@@ -16,42 +16,44 @@
 package vm
 
 import (
+	"fmt"
+	"github.com/taiyuechain/taiyuechain/common"
+	"github.com/taiyuechain/taiyuechain/crypto"
 	"math/big"
 	"testing"
-	"github.com/taiyuechain/taiyuechain/common"
-	"fmt"
-	"github.com/taiyuechain/taiyuechain/crypto"
 )
 
-var(
-	pbft1PrivString ="7631a11e9d28563cdbcf96d581e4b9a19e53ad433a53c25a9f18c74ddf492f75"
-	pbft2PrivString ="bab8dbdcb4d974eba380ff8b2e459efdb6f8240e5362e40378de3f9f5f1e67bb"
-	pbft3PrivString ="122d186b77a030e04f5654e13d934b21af2aac03b942c3ecda4632364d81cbab"
-	pbft4PrivString ="fe44cbc0e164092a6746bd57957422ab165c009d0299c7639a2f4d290317f20f"
-	rootList  []common.Address
-	)
-func init(){
-	SetConfig(true,true)
+var (
+	pbft1PrivString = "7631a11e9d28563cdbcf96d581e4b9a19e53ad433a53c25a9f18c74ddf492f75"
+	pbft2PrivString = "bab8dbdcb4d974eba380ff8b2e459efdb6f8240e5362e40378de3f9f5f1e67bb"
+	pbft3PrivString = "122d186b77a030e04f5654e13d934b21af2aac03b942c3ecda4632364d81cbab"
+	pbft4PrivString = "fe44cbc0e164092a6746bd57957422ab165c009d0299c7639a2f4d290317f20f"
+	rootList        []common.Address
+	root1           common.Address
+	member1         common.Address
+	member2         common.Address
+	ptable          *PerminTable
+)
+
+func init() {
+	SetConfig(true, true)
 
 	rootList = append(rootList, common.HexToAddress("0x21C16f03bbF085D6908569d159Ad40BcafdB80C5"))
 	rootList = append(rootList, common.HexToAddress("0xa9A2CbA5d5d16DE370375B42662F3272279B2b89"))
 	rootList = append(rootList, common.HexToAddress("0x6bE9780954580FCC268944e9D6271B3Dfc886997"))
 	rootList = append(rootList, common.HexToAddress("0x03096816367827E9C5c1993AE18b237895717500"))
-}
 
-func TestPerminTable_GrantPermission(t *testing.T) {
-
-	//add send tx
-	ptable := NewPerminTable()
+	ptable = NewPerminTable()
 	ptable.InitPBFTRootGrop(rootList)
 
-	root1 := rootList[1]
-	member1 :=common.HexToAddress("0xf22142DbF24C324Eb021332c2D673d3B819B955a")
-	member2 :=common.HexToAddress("0xFE9cFAc0EDf17FB746069f1d12885217fF30234C")
+	root1 = rootList[1]
+	member1 = common.HexToAddress("0xf22142DbF24C324Eb021332c2D673d3B819B955a")
+	member2 = common.HexToAddress("0xFE9cFAc0EDf17FB746069f1d12885217fF30234C")
+}
 
+func TestPerminTable_DeletePermission(t *testing.T) {
 	// check no permission account
 	if ptable.CheckActionPerm(common.BytesToAddress([]byte("1234")),common.Address{},common.Address{},PerminType_SendTx){
-
 		t.Fatalf("CheckActionPerm err PerminType_SendTx")
 	}
 
@@ -62,7 +64,47 @@ func TestPerminTable_GrantPermission(t *testing.T) {
 	}
 
 	if !ptable.CheckActionPerm(member1,common.Address{},common.Address{},PerminType_SendTx){
+		t.Fatalf("CheckActionPerm err PerminType_SendTx")
+	}
 
+	res, err =ptable.GrantPermission(member1,root1,member1,common.Address{},ModifyPerminType_CrtGrop,"a",true)
+	if !res {
+		fmt.Println(err)
+		t.Fatalf("Grent err,ModifyPerminType_AddCrtContractManagerPerm")
+	}
+
+	gropAddr := crypto.CreateGroupkey(member1,3)
+	//ModifyPerminType_AddGropManagerPerm
+	res, err =ptable.GrantPermission(member1,root1,member2,gropAddr,ModifyPerminType_AddGropManagerPerm,"a",true)
+	if !res {
+		fmt.Println(err)
+		t.Fatalf("Grent err,ModifyPerminType_AddCrtContractManagerPerm")
+	}
+	if !ptable.CheckActionPerm(member2,gropAddr,common.Address{},ModifyPerminType_DelGrop){
+		t.Fatalf("CheckActionPerm err ModifyPerminType_DelGrop")
+	}
+
+	//ModifyPerminType_DelGropManagerPerm
+	res, err =ptable.GrantPermission(member1,root1,member2,gropAddr,ModifyPerminType_DelGropManagerPerm,"a",true)
+	if !res {
+		fmt.Println(err)
+		t.Fatalf("Grent err,ModifyPerminType_AddCrtContractManagerPerm")
+	}
+
+	res, err = ptable.GrantPermission(member2, root1, member2, gropAddr, ModifyPerminType_DelGrop, "a", true)
+	if !res {
+		t.Fatalf("Grent err,ModifyPerminType_DelGrop")
+	}
+}
+
+func TestPerminTable_GrantPermission(t *testing.T) {
+	res, err :=ptable.GrantPermission(root1,root1,member1,common.Address{},ModifyPerminType_AddSendTxPerm,"a",true)
+	if !res{
+		fmt.Println(err)
+		t.Fatalf("Grent err")
+	}
+
+	if !ptable.CheckActionPerm(member1,common.Address{},common.Address{},PerminType_SendTx){
 		t.Fatalf("CheckActionPerm err PerminType_SendTx")
 	}
 
@@ -100,9 +142,8 @@ func TestPerminTable_GrantPermission(t *testing.T) {
 		fmt.Println(err)
 		t.Fatalf("Grent err,ModifyPerminType_AddCrtContractManagerPerm")
 	}
-	if ptable.CheckActionPerm(member2,gropAddr,common.Address{},ModifyPerminType_DelGrop){
-
-		t.Fatalf("CheckActionPerm err ModifyPerminType_DelGrop")
+	if !ptable.CheckActionPerm(member2,gropAddr,common.Address{},PerminType_SendTx){
+		t.Fatalf("CheckActionPerm err PerminType_SendTx")
 	}
 	//ModifyPerminType_DelGropMemberPerm
 	res, err =ptable.GrantPermission(member1,root1,member2,gropAddr,ModifyPerminType_DelGropMemberPerm,"a",true)
@@ -110,14 +151,12 @@ func TestPerminTable_GrantPermission(t *testing.T) {
 		fmt.Println(err)
 		t.Fatalf("Grent err,ModifyPerminType_AddCrtContractManagerPerm")
 	}
-	if ptable.CheckActionPerm(member2,gropAddr,common.Address{},ModifyPerminType_DelGrop){
-
-		t.Fatalf("CheckActionPerm err ModifyPerminType_DelGrop")
+	if ptable.CheckActionPerm(member2,gropAddr,common.Address{},PerminType_SendTx){
+		t.Fatalf("CheckActionPerm err PerminType_SendTx")
 	}
 
 
 	if !ptable.CheckActionPerm(member1,gropAddr,common.Address{},ModifyPerminType_DelGrop){
-
 		t.Fatalf("CheckActionPerm err ModifyPerminType_DelGrop")
 	}
 	res, err =ptable.GrantPermission(member1,root1,member1,gropAddr,ModifyPerminType_DelGrop,"a",true)
@@ -129,8 +168,6 @@ func TestPerminTable_GrantPermission(t *testing.T) {
 
 		t.Fatalf("CheckActionPerm err ModifyPerminType_DelGrop")
 	}
-
-
 
 	res, err =ptable.GrantPermission(root1,root1,member1,common.Address{},ModifyPerminType_DelSendTxPerm,"a",true)
 	if !res{
